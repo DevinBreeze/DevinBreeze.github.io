5feb09ddcb89dc351f4f5a2da6247ca783426403
Devin Breazeale <breazeale.devin@gmail.com>
2019-06-28 17:06:16 +0000

    Adding Snake and Runtime Folder

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,30 +1,30 @@
-var init = function (window) {
+var init = function(window) {
     'use strict';
-    var 
+    var
         draw = window.opspark.draw,
         physikz = window.opspark.racket.physikz,
-        
+
         app = window.opspark.makeApp(),
-        canvas = app.canvas, 
+        canvas = app.canvas,
         view = app.view,
         fps = draw.fps('#000');
-        
-    
+
+
     window.opspark.makeGame = function() {
-        
+
         window.opspark.game = {};
         var game = window.opspark.game;
-        
+
         ////////////////////////////////////////////////////////////////
         // ALL CODE GOES BELOW HERE                                   //
         ////////////////////////////////////////////////////////////////
-        
+
         // TODO 1 : Declare and initialize our variables //
         var circle;
         var circles = [];
         var i;
         //put in for loop
-        var eachCircle = circles[i];
+
 
         // TODO 2 : Create a function that draws a circle  //
         function drawCircle() {
@@ -35,67 +35,68 @@ var init = function (window) {
         }
 
         // TODO 3 : Call the drawCircle function 5 times //
-        for (i = 0; i < 5; i++) {
+        for (i = 0; i < 100; i++) {
             drawCircle();
         }
 
         view.addChild(fps);
         app.addUpdateable(fps);
-    
+
         game.checkCirclePosition = function(circle) {
             // TODO 5 : YOUR CODE STARTS HERE //////////////////////
-            
+
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
-            } 
+            }
             // TODO 5a) if the circle has gone past of the LEFT side of the screen then place it on the RIGHT
-            else if (circle.x < canvas.width ) {
+            else if (circle.x < 0) {
                 circle.x = canvas.width;
-            } 
+            }
 
             // TODO 5b) if the circle has gone past of the TOP side of the screen then place it on the BOTTOM
-            if ( circle.y < canvas.hieght ) {
+            if (circle.y < 0) {
                 circle.y = canvas.hieght;
             }
             // TODO 5c) if the circle has gone past of the BOTTOM side of the screen then place it OFF-SCREEN TOP
-            else if ( circle.y > canvas.hieght ) {
+            else if (circle.y > canvas.hieght) {
                 circle.y = 0;
             }
             // YOUR TODO 5 CODE ENDS HERE //////////////////////////
         }
-    
+
         function update() {
             // TODO 4 : Update the circle's position //
-                physikz.updatePosition(eachCircle);
-            
+
             // TODO 5 : Call game.checkCirclePosition on your circles.
-                game.checkCirclePosition(eachCircle);
 
             // TODO 8 : Iterate over the array
-           var circles = [];
-           
-           for (i = 0; i < circles.length; i++) {
-               eachCircle;
-           }
+
+            // var circles = [];
+
+            for (i = 0; i < circles.length; i++) {
+                var eachCircle = circles[i];
+                physikz.updatePosition(eachCircle); // put this in the loop too
+                game.checkCirclePosition(eachCircle); // put this in the loop too
+            }
 
         }
-        
+
         ////////////////////////////////////////////////////////////////////
         // NO CODE BELOW HERE                                             //
         ////////////////////////////////////////////////////////////////////
-        
+
         game.circle = circle;
         game.circles = circles;
         game.drawCircle = drawCircle;
         game.update = update;
-        
+
         app.addUpdateable(window.opspark.game);
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
